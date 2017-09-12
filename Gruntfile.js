f84d115fb27a1c598d9e5d8b35a44ca288668a3c
Imrich Lakatos <lakatos.imro@gmail.com>
2017-09-12 16:13:31 +0200

    Remove unnecessary tasks

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt){
+    // Project configuration.
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy:{
+            js: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile',
+                        src: ['src/js/main.js'],
+                        dest: 'dist/js'
+                    }
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/js/main.js','src/scss/main.scss'],
+                tasks: ['copy','sass']
+            },
+            options: {
+                reload: true
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'dist/main.css': 'src/scss/main.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.registerTask('default', ['copy', 'concat','sass']);
+};
